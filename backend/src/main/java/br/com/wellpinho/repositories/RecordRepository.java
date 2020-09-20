b1536abd48b69aba9c243c9aa42d5476cdae8225
wellpinho <wellpinho@outlook.com>
2020-09-20 19:10:42 -0300

    GET records

--- backend/src/main/java/br/com/wellpinho/repositories/RecordRepository.java
+++ backend/src/main/java/br/com/wellpinho/repositories/RecordRepository.java
@@ -3,7 +3,6 @@ package br.com.wellpinho.repositories;
 import java.time.Instant;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
