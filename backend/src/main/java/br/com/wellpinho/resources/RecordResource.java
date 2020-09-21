eda4de20bd44cda8c7326d82aa70d8553926f844
wellpinho <wellpinho@outlook.com>
2020-09-21 00:22:52 -0300

    Removido imports sem uso

--- backend/src/main/java/br/com/wellpinho/resources/RecordResource.java
+++ backend/src/main/java/br/com/wellpinho/resources/RecordResource.java
@@ -1,7 +1,6 @@
 package br.com.wellpinho.resources;
 
 import java.time.Instant;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
